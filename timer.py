6477d4977eb0b8c70a397bd381b20082a8db68b7
Muhad99 <145128166+Muhad99@users.noreply.github.com>
2024-01-15 20:17:44 +0100

    add it

--- timer.py
+++ timer.py
@@ -1,13 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_bootstrap import Bootstrap
 from datetime import datetime 
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 
 
 app = Flask(__name__)
 
+app.secret_key = 'key'
+
+# Enter you database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'text'
+
+# Intialize MYSQL
+mysql = MySQL(app)
+
 
 bootstrap = Bootstrap(app)
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO student VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
+
+@app.route('/log', methods =['GET', 'POST'])
+def log():
+    msg = ''
+    
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM student WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['Id'] = account['Id']
+            session['Username'] = account['Username']
+            msg = 'Logged in successfully !'
+            return render_template('home.html', msg = 'username')
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
 
 @app.route('/')
 def index():
